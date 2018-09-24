a27f7ab5e219d84afe40d58d3d27ea47d5249dd1
PavelV95 <ehalzombi@gmail.com>
2018-09-24 13:14:50 +0300

    #1 Refactor url generation method

--- Nop.Plugin.DiscountRules.ShippingCountry/ShippingCountryDiscountRequirementRule.cs
+++ Nop.Plugin.DiscountRules.ShippingCountry/ShippingCountryDiscountRequirementRule.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
@@ -75,8 +76,14 @@ namespace Nop.Plugin.DiscountRules.ShippingCountry
         public string GetConfigurationUrl(int discountId, int? discountRequirementId)
         {
             var urlHelper = _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext);
-            return urlHelper.Action("Configure", "DiscountRulesShippingCountry",
-                new { discountId = discountId, discountRequirementId = discountRequirementId }).TrimStart('/');
+            var url = new PathString(urlHelper.Action("Configure", "DiscountRulesShippingCountry",
+                new { discountId = discountId, discountRequirementId = discountRequirementId }));
+
+            //remove the application path from the generated URL if exists
+            var pathBase = _actionContextAccessor.ActionContext?.HttpContext?.Request?.PathBase ?? PathString.Empty;
+            url.StartsWithSegments(pathBase, out url);
+
+            return url.Value.TrimStart('/');
         }
 
         public override void Install()
